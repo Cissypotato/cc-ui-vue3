816f0d4a0a6c85e9ce1ffa7a45dbc4b71ae2249a
Cissy <1395422644@qq.com>
2020-09-11 23:25:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <CC />
+</template>
+
+<script>
+import CC from './components/CC.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CC
+  }
+}
+</script>
