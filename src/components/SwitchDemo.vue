7b9f088b72d050ba8eeba3c04ef6122db5f92254
Cissy <1395422644@qq.com>
2020-09-16 23:10:16 +0800

    switch component

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="switch-wrapper">
         
-        <switch1> </switch1>
+        <switch1 v-model:value="y"> </switch1>
     </div>
 </template>
 <script lang="ts">
+import { ref } from 'vue'
 //  import Switch from '../libs/Switch.vue.vue'
  import switch1 from '../libs/switch1.vue'
 
@@ -12,8 +13,14 @@
 export default {
     name:"switchDemo",
     components:{
-       
         switch1
+    },
+    setup(){
+        const y=ref(false)
+        // const b=(event)=>{
+        //     console.log(event)
+        // }
+        return{y}
     }
 }
 </script>
--- src/libs/switch1.vue
+++ src/libs/switch1.vue
@@ -1,10 +1,59 @@
 <template>
     <div>
-        abc
+        <button :class="{checked:value}" @click="toggle"><span ></span></button>
     </div>
 </template>
 <script>
+import { ref } from 'vue'
 export default {
-    name:"switch"
+    name:"switch1",
+    props:{
+        value:Boolean
+    },
+    setup(props,context){
+        const toggle=()=>{
+            context.emit('update:value',!props.value)
+        }
+        return{toggle}
+    }
 }
 </script>
+<style lang="scss" scoped>
+$h1:22px;
+$h2:$h1 - 4px;
+$h3:$h2+2px;
+$mainColor:blue;
+$defaultColor:gray;
+button:focus{outline: none;}
+button{
+    height: $h1;
+    width: $h1*2;
+    border-radius: $h1;
+    background: $defaultColor;
+    border: none;
+    position: relative; 
+}
+span{
+        height: $h2;
+        width: $h2;
+        border-radius: $h2/2;
+        background: #fff;
+        position: absolute;
+        top: 2px;
+        left: 2px;
+        transition: left 250ms;
+    }
+    button.checked{
+        background: $mainColor;
+    }
+    button.checked span{
+        
+        left:calc(100% - #{$h3});
+    }
+
+// button:hover > span{
+//         left: calc(100% - #{$h2}-2px);
+//     }
+
+
+</style>
