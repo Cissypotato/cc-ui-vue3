b1b6a54b8a385c84615b964d1cfc7f17df7c130f
Cissy <1395422644@qq.com>
2020-10-29 23:55:43 +0800

    首页装修

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,66 +1,68 @@
 <template>
-    <div class="top-nav">
-        <div class="logo" >CC-UI</div>
-        <div class="navs">
-            <a href="">nav1</a>
-            <a href="">nav2</a>
-        </div>
-        <span class="toggleAside" @click="toggleVisible">SSS</span>
+  <div class="top-nav">
+    <div class="logo">
+      <svg class="icon">
+        <use xlink:href="#icon-cc1"></use>
+      </svg>
     </div>
+    <div class="navs">
+      <a href="">nav1</a>
+      <a href="">nav2</a>
+    </div>
+    <span class="toggleAside" @click="toggleVisible">SSS</span>
+  </div>
 </template>
 <script lang="ts">
-import { inject, Ref } from 'vue'
+import { inject, Ref } from "vue";
 export default {
-    name:'top-nav',
-    setup(){
-        const asideVisible=inject<Ref<boolean>>('abc')
-        const toggleVisible=()=>{
-            console.log(asideVisible.value)
-            asideVisible.value=!asideVisible.value
-        }
-        return{ toggleVisible}
-    }
-}
+  name: "top-nav",
+  setup() {
+    const asideVisible = inject<Ref<boolean>>("abc");
+    const toggleVisible = () => {
+      console.log(asideVisible.value);
+      asideVisible.value = !asideVisible.value;
+    };
+    return { toggleVisible };
+  },
+};
 </script>
 <style lang="scss" scoped>
-    .top-nav{
-        display: flex;
-        height: 3.6rem;
-        justify-content: space-between;
-        align-items: center;
-        line-height: 3.6rem;
-        background: #fff;
-        border: 1px solid red;
-        position: fixed;
-        width: 100%;
-        top: 0;
-        left: 0;
-        z-index: 10;
-        .logo{
-            font-weight: 600;
-            font-size:1.25rem;
-            margin-left: 20px;
-        }
-        .navs{
-            a{
-                margin-right: 20px;
-            }
-        }
-        .toggleAside{
-            display: none;
-        }
-        @media (max-width: 500px) {
-            >.navs{
-                display: none;
-            }
-            >.logo{
-                margin: 0 auto;
-            }
-            >.toggleAside{
-                display: block;
-                position: absolute;
-                left: 20px;
-            }
-        }
+.top-nav {
+  display: flex;
+  height: 3.6rem;
+  justify-content: space-between;
+  align-items: center;
+  line-height: 3.6rem;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  .logo {
+    font-weight: 600;
+    font-size: 1.25rem;
+    margin-left: 20px;
+  }
+  .navs {
+    a {
+      margin-right: 20px;
     }
+  }
+  .toggleAside {
+    display: none;
+  }
+  @media (max-width: 500px) {
+    > .navs {
+      display: none;
+    }
+    > .logo {
+      margin: 0 auto;
+    }
+    > .toggleAside {
+      display: block;
+      position: absolute;
+      left: 20px;
+    }
+  }
+}
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
+<div>
+  <div class="nav-and-banner">
     <top-nav />
     <div class="banner">
       <h1>CC-UI</h1>
@@ -7,11 +8,22 @@
       <div class="actions">
             <a href="https://github.com/Cissypotato/cc-ui-vue3">GitHub</a>
             <router-link to="/doc">开始</router-link>
-      </div>
-      
+      </div>   
     </div>
-    
   </div>
+  <div class="features">
+     <svg class="icon">
+        <use xlink:href="#icon-Vue"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-typescript"></use>
+      </svg>
+      <svg class="icon">
+        <use xlink:href="#icon-dengpao"></use>
+      </svg>
+  
+  </div>
+</div>
 </template>
 <script lang="ts">
 import TopNav from "../components/TopNav.vue";
@@ -24,18 +36,30 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.nav-and-banner{
+  color:#769bf0;
+  background: rgb(212,211,227);
+  background: linear-gradient(45deg, rgba(212,211,227,1) 0%, rgba(244,243,248,1) 100%);
+  clip-path: ellipse(80% 60% at 50% 40%);
+}
+.features{
+  >svg{
+    width:64px;
+    height: 64px;
+  }
+}
 .banner {
   padding: 100px 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  background: lightgreen;
   > .actions {
     padding: 8px 0;
     a {
       margin: 0 8px;
-      background: #fff;
+      background:#1819ed;
+      color: #fff;
       display: inline-block;
       $h: 28px;
       height: $h;
